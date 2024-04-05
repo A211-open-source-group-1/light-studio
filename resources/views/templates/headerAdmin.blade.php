f37854a42b4d41f278449cb5accf8b55b2474df1
Bjkdy <pv198357@gmail.com>
2024-04-05 12:31:44 +0700

    add jquery header admin

--- resources/views/templates/headerAdmin.blade.php
+++ resources/views/templates/headerAdmin.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="{{asset('/css/bootstrap.min.css')}}">
-    <script src="{{asset('/js/bootstrap.bundle.min.js')}}"></script>
     <link rel="stylesheet" href="{{asset('/css/admincss.css')}}">
+    <script src="{{asset('/js/bootstrap.bundle.min.js')}}"></script>
     <script src="{{asset('/js/sidebarAdmin.js')}}"></script>
 
 
